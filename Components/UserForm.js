ce4a8aaee94309b06a567f0df666a84b525b3c51
DESKTOP-1RVG80B\evgen <nightromantic63@gmail.com>
2021-07-20 17:14:18 +0400

    Finish

--- Components/UserForm.js
+++ Components/UserForm.js
@@ -1,17 +1,12 @@
 import React, { useState } from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 import * as Yup from 'yup';
 
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// Wizard is a single Formik instance whose children are each page of the
-// multi-step form. The form is submitted on each forward transition (can only
-// progress with valid input), whereas a backwards step is allowed with
-// incomplete data. A snapshot of form state is used as initialValues after each
-// transition. Each page has an optional submit handler, and the top-level
-// submit is called when the final page is submitted.
 const Wizard = ({ children, initialValues, onSubmit }) => {
   const [stepNumber, setStepNumber] = useState(0);
   const steps = React.Children.toArray(children);
@@ -120,6 +115,7 @@ const UserForm = () => (
         <div style={{ display: "flex", flexDirection: "column" }}>
           <label htmlFor="email">Email</label>
           <Field
+            as={FormControl}
             id="email"
             name="email"
             placeholder="Email"
@@ -136,6 +132,7 @@ const UserForm = () => (
         >
           <label htmlFor="password">Password</label>
           <Field
+            as={FormControl}
             id="password"
             name="password"
             placeholder="Password"
@@ -152,6 +149,7 @@ const UserForm = () => (
         >
           <label htmlFor="confirmPass">Confirm password</label>
           <Field
+            as={FormControl}
             id="confirmPass"
             name="confirmPass"
             placeholder="Confirm password"
@@ -166,11 +164,11 @@ const UserForm = () => (
         <div style={{ display: "flex", flexDirection: "column" }}>
           <label htmlFor="birthday">Birthday</label>
           <Field
+            as={FormControl}
             id="birthday"
             name="birthday"
             type="date"
           />
-          <ErrorMessage className="error" component="div" name="birthday" />
         </div>
         <div
           style={{
@@ -181,12 +179,12 @@ const UserForm = () => (
         >
           <label htmlFor="phone">Phone</label>
           <Field
+            as={FormControl}
             id="phone"
             name="phone"
             placeholder="Phone"
             type="text"
           />
-          <ErrorMessage className="error" component="div" name="phone" />
         </div>
         <div
           style={{
@@ -197,12 +195,12 @@ const UserForm = () => (
         >
           <label htmlFor="country">Country</label>
           <Field
+            as={FormControl}
             id="country"
             name="country"
             placeholder="Country"
             type="text"
           />
-          <ErrorMessage className="error" component="div" name="country" />
         </div>
       </WizardStep>
       <WizardStep
@@ -211,12 +209,12 @@ const UserForm = () => (
         <div style={{ display: "flex", flexDirection: "column" }}>
           <label htmlFor="avatar">Avatar</label>
           <Field
+            as={FormControl}
             id="avatar"
             name="avatar"
             type="text"
             placeholder="Avatar"
           />
-          <ErrorMessage className="error" component="div" name="avatar" />
         </div>
         <div
           style={{
@@ -227,12 +225,12 @@ const UserForm = () => (
         >
           <label htmlFor="status">Status</label>
           <Field
+            as={FormControl}
             id="status"
             name="status"
             placeholder="Status"
             type="text"
           />
-          <ErrorMessage className="error" component="div" name="status" />
         </div>
         <div
           style={{
@@ -243,12 +241,12 @@ const UserForm = () => (
         >
           <label htmlFor="about">About</label>
           <Field
+            as={FormControl}
             id="about"
             name="about"
             placeholder="About"
             type="text"
           />
-          <ErrorMessage className="error" component="div" name="about" />
         </div>
       </WizardStep>
     </Wizard>
